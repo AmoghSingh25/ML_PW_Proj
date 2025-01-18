f077ac1ca3b9eeed4f6760f1cb0d58785e452dd2
Sonia Koszut <sonia@ollmoo.com>
2025-01-18 14:53:14 +0000

    optimise for barrier placement

--- simulation.py
+++ simulation.py
@@ -96,10 +96,11 @@ class simulation:
         wave = self.DIM_MAP - wave
         wave = self.calculate_wave_pos(wave)
         return wave
-    
+        
     def place_obstacles(self):
         for i in self.obstacle_coords:
-            self.obstacle_map[i[0],i[1]] = 1
+            self.obstacle_map[int(i[0]), int(i[1])] = 1
+
 
     def display_map(self, coast, stream=True, openCV=False):
         cv.namedWindow("Coast")
